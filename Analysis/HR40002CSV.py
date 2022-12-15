2677b184b2ef0aee7e8b5da7f11894a50566636d
mitchem890 <mitchem890@gmail.com>
2022-12-15 14:04:45 -0700

    Update HR40002CSV.py
    Fixed issues with HR4000 Data generation

--- Analysis/HR40002CSV.py
+++ Analysis/HR40002CSV.py
@@ -6,38 +6,37 @@ import struct
 from pathlib import Path
 from scipy.signal import savgol_filter
 
-##
-paths = Path('/users/mjeffers/Desktop/IS1/').glob('*')
-window=5 # This is a window of 5
+
+########################EDIT PATH HERE###############################################################
+paths = Path('C:\\HAO-IG\\DIMS\\Testing\\10 Minute Test 7-Dec-2022\\IS1').glob('*')
 #go through Paths
 for path in paths:
 
     #Find the Correct files starting with n00
     files = Path(path).glob('**/n00*.txt')
-    #create an array of Zeros
-    nsum = np.zeros(3648)
-    matplotlib.use('Agg')
     for file in files:
+        print(f"Processing File {file}")
         spc=[]
-        blk=[]
+        #Open The File and Read in the Data
         with open(file, 'rb') as f:
-            print(file)
-
-            f.read(9)
-            data=f.read(4) # integration time
-            integration_time=data[2]*256+data[3]+256*256*(data[0]*256+data[1])#Find the integration_time in us
-            f.read(2) # pixel mode
-            f.read(5*2) # not usable
-            for i in range(13):
+            #Read in the First 14 Bytes
+            data=f.read(14)
+            while True:
+                #Read int the next 2 Bytes
                 data=f.read(2)
-                blk.append(struct.unpack(">H", data)[0])
-            f.read(3*2) # transition pixels
-            #Read in the Data
-            for i in range(3648):
-                data=f.read(2)
-                spc.append(struct.unpack(">H", data)[0])
+                if not data:
+                    break
+                #Unpack the Data
+                spc.append(struct.unpack("H", data)[0])
+            #Write to the CSV
             with open(f'{str(file).strip(".txt")}.csv', 'w', encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
                 # write multiple rows
-                writer.writerow(data)
+                writer.writerow(spc)
+            #Plot the Data
+            plt.plot(spc)
+            plt.title(file)
+            plt.savefig(file.with_suffix('.png'))
+            plt.clf()
+
 
